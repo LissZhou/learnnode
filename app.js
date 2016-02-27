0167e7461f1b0731b0c3a9d20e81756bf76c78ed
yongjz <jingzhouyong@jingzhoudeMacBook-Pro.local>
2016-02-28 00:19:30 +0800

    update all packages

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-var express = require('express')
-var path = require('path')
-var mongoose = require('mongoose')
-var mongoStore = require('connect-mongo')(express)
-var port = process.env.PORT || 3000
-var app = express()
-var fs = require('fs')
-var dbUrl = 'mongodb://localhost/test'
+var express = require('express');
+var session = require('express-session')
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var path = require('path');
+var mongoose = require('mongoose');
+var mongoStore = require('connect-mongo')(session);
+var port = process.env.PORT || 3000;
+var app = express();
+var fs = require('fs');
+var dbUrl = 'mongodb://localhost/imooc'
 
-mongoose.connect(dbUrl)
+mongoose.connect(dbUrl);
 
 // models loading
 var models_path = __dirname + '/app/models'
@@ -28,31 +31,41 @@ var walk = function(path) {
       }
     })
 }
-walk(models_path)
-app.set('views', './app/views/pages')
-app.set('view engine', 'jade')
-app.use(express.bodyParser())
-app.use(express.cookieParser())
-app.use(express.multipart())
-app.use(express.session({
+walk(models_path);
+app.set('views', './app/views/pages');
+app.set('view engine', 'jade');
+// app.use(express.bodyParser());
+app.use(bodyParser.json({limit: '1mb'}));
+app.use(bodyParser.urlencoded({ extended: true, limit: '1mb' }));
+app.use(cookieParser());
+//app.use(express.multipart());
+app.use(session({
   secret: 'imooc',
   store: new mongoStore({
     url: dbUrl,
     collection: 'sessions'
   })
-}))
+}));
+// app.use(session({
+//   secret: 'keyboard cat',
+//   resave: false,
+//   saveUninitialized: true,
+//   cookie: { secure: true }
+// }))
 
 if ('development' === app.get('env')) {
   app.set('showStackError', true)
-  app.use(express.logger(':method :url :status'))
+  //app.use(express.logger(':method :url :status'))
   app.locals.pretty = true
   //mongoose.set('debug', true)
 }
 
-require('./config/routes')(app)
+//require('./config/routes')(app)
+var webRouter = require('./config/routes');
+app.use('/', webRouter);
 
-app.listen(port)
-app.locals.moment = require('moment')
-app.use(express.static(path.join(__dirname, 'public')))
+app.listen(port);
+app.locals.moment = require('moment');
+app.use(express.static(path.join(__dirname, 'public')));
 
-console.log('started on port ' + port)
+console.log('started on port ' + port);
