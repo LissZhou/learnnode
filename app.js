624fee7b1e143520f5d13e9b034505acdfaf6d15
yongjz <jingzhouyong@jingzhoudeMacBook-Pro.local>
2016-02-28 18:29:49 +0800

    update express3 to 4

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ var express = require('express');
 var session = require('express-session')
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
+var multipart = require('connect-multiparty');
+var logger = require('morgan');
 var path = require('path');
 var mongoose = require('mongoose');
 var mongoStore = require('connect-mongo')(session);
 var port = process.env.PORT || 3000;
 var app = express();
 var fs = require('fs');
-var dbUrl = 'mongodb://localhost/imooc'
 
+var dbUrl = 'mongodb://localhost/test'
 mongoose.connect(dbUrl);
 
 // models loading
@@ -18,27 +20,26 @@ var walk = function(path) {
   fs
     .readdirSync(path)
     .forEach(function(file) {
-      var newPath = path + '/' + file
-      var stat = fs.statSync(newPath)
+      var newPath = path + '/' + file;
+      var stat = fs.statSync(newPath);
 
       if (stat.isFile()) {
         if (/(.*)\.(js|coffee)/.test(file)) {
-          require(newPath)
+          require(newPath);
         }
       }
       else if (stat.isDirectory()) {
-        walk(newPath)
+        walk(newPath);
       }
     })
 }
 walk(models_path);
 app.set('views', './app/views/pages');
 app.set('view engine', 'jade');
-// app.use(express.bodyParser());
 app.use(bodyParser.json({limit: '1mb'}));
 app.use(bodyParser.urlencoded({ extended: true, limit: '1mb' }));
 app.use(cookieParser());
-//app.use(express.multipart());
+app.use(multipart());
 app.use(session({
   secret: 'imooc',
   store: new mongoStore({
@@ -46,26 +47,19 @@ app.use(session({
     collection: 'sessions'
   })
 }));
-// app.use(session({
-//   secret: 'keyboard cat',
-//   resave: false,
-//   saveUninitialized: true,
-//   cookie: { secure: true }
-// }))
 
 if ('development' === app.get('env')) {
-  app.set('showStackError', true)
-  //app.use(express.logger(':method :url :status'))
-  app.locals.pretty = true
+  app.set('showStackError', true);
+  app.use(logger(':method :url :status'));
+  app.locals.pretty = true;
   //mongoose.set('debug', true)
 }
 
-//require('./config/routes')(app)
-var webRouter = require('./config/routes');
-app.use('/', webRouter);
+var routes = require('./config/routes');
+app.use('/', routes);
 
 app.listen(port);
 app.locals.moment = require('moment');
 app.use(express.static(path.join(__dirname, 'public')));
 
-console.log('started on port ' + port);
+console.log('website started on port ' + port);
--- config/routes.js
+++ config/routes.js
@@ -8,33 +8,33 @@ var Category = require('../app/controllers/category')
 var router = express.Router();
 
 // Index
-router.get('/', Index.index)
+router.get('/', Index.index);
 
 // User
-router.post('/user/signup', User.signup)
-router.post('/user/signin', User.signin)
-router.get('/signin', User.showSignin)
-router.get('/signup', User.showSignup)
-router.get('/logout', User.logout)
-router.get('/admin/user/list', User.signinRequired, User.adminRequired, User.list)
+router.post('/user/signup', User.signup);
+router.post('/user/signin', User.signin);
+router.get('/signin', User.showSignin);
+router.get('/signup', User.showSignup);
+router.get('/logout', User.logout);
+router.get('/admin/user/list', User.signinRequired, User.adminRequired, User.list);
 
 // Movie
-router.get('/movie/:id', Movie.detail)
-router.get('/admin/movie/new', User.signinRequired, User.adminRequired, Movie.new)
-router.get('/admin/movie/update/:id', User.signinRequired, User.adminRequired, Movie.update)
-router.post('/admin/movie', User.signinRequired, User.adminRequired, Movie.savePoster, Movie.save)
-router.get('/admin/movie/list', User.signinRequired, User.adminRequired, Movie.list)
-router.delete('/admin/movie/list', User.signinRequired, User.adminRequired, Movie.del)
+router.get('/movie/:id', Movie.detail);
+router.get('/admin/movie/new', User.signinRequired, User.adminRequired, Movie.new);
+router.get('/admin/movie/update/:id', User.signinRequired, User.adminRequired, Movie.update);
+router.post('/admin/movie', User.signinRequired, User.adminRequired, Movie.savePoster, Movie.save);
+router.get('/admin/movie/list', User.signinRequired, User.adminRequired, Movie.list);
+router.delete('/admin/movie/list', User.signinRequired, User.adminRequired, Movie.del);
 
 // Comment
-router.post('/user/comment', User.signinRequired, Comment.save)
+router.post('/user/comment', User.signinRequired, Comment.save);
 
 // Category
-router.get('/admin/category/new', User.signinRequired, User.adminRequired, Category.new)
-router.post('/admin/category', User.signinRequired, User.adminRequired, Category.save)
-router.get('/admin/category/list', User.signinRequired, User.adminRequired, Category.list)
+router.get('/admin/category/new', User.signinRequired, User.adminRequired, Category.new);
+router.post('/admin/category', User.signinRequired, User.adminRequired, Category.save);
+router.get('/admin/category/list', User.signinRequired, User.adminRequired, Category.list);
 
 // results
-router.get('/results', Index.search)
+router.get('/results', Index.search);
 
 module.exports = router;
